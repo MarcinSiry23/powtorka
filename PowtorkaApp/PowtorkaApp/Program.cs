d786e1a54114bd70bd27e10341d729b2d8ea8e8e
Marcin Siry <sirymarcin@gmail.com>
2024-02-28 22:54:58 +0000

    day no. 5

--- PowtorkaApp/PowtorkaApp/Program.cs
+++ PowtorkaApp/PowtorkaApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PowtorkaApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę: ");
+        var numbers = Console.ReadLine();
+        char[] letters = numbers.ToArray();
+        List<char> digits = new() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        foreach (char digit in digits) 
+        {
+            int counter = 0;
+            foreach (var letter in letters) 
+            {
+                if (digit == letter)
+                {  counter++; }
+            }
+            Console.WriteLine($"Liczba {digit} występuje {counter} razy");
+        }
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    int counter = 0;
+        //    foreach (var letter in letters)
+        //    {
+        //        int charInNumber = letter - '0';
+        //        if (charInNumber == i)
+        //        {
+        //            counter++;
+        //        }
+
+        //    }
+        //    Console.WriteLine("Liczba "+ i + " występuje " + counter+ " razy");
+        //}
+
+    }
+}
